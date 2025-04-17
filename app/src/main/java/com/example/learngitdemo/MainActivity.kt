1ae2a08e34598b3fa3b33d5b6c783061853a37b0
Monika0103 <monikagupta433@gmail.com>
2025-04-17 06:49:06 +0530

    main first commit

--- app/src/main/java/com/example/learngitdemo/MainActivity.kt
+++ app/src/main/java/com/example/learngitdemo/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learngitdemo.ui.theme.LearnGitDemoTheme
 
+//main first commit
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
